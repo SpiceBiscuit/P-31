168e90088b97b4150ab3d82f5622c3d89f6c104b
SpiceBiscuit <72450107+SpiceBiscuit@users.noreply.github.com>
2021-06-08 19:51:39 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,20 @@
+//I have to create the class here
+class Particle{
+    constructor(x, y,radius, angle) {
+        var options = {
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.circle(x, y, radius, options);
+        this.radius = radius;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        ellipseMode(RADIUS);
+        ellipse(0, 0, this.radius, this.radius);
+        pop();
+      }
+}
